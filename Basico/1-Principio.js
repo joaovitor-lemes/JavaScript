4fbb2d03abd16948f4291a625dacc8f93a3db0a7
Joao Vitor <joaovitorlemes85@gmail.com>
2025-05-24 15:12:40 -0300

    Commitando alterações

--- Basico/1-Principio.js
+++ Basico/1-Principio.js
@@ -1,4 +1,11 @@
-let nome = "João";
-let idade = 20;
 
-console.log("meu nome é " + nome + " e tenho " + idade + " anos");
+/* No caso de variáveis, utilizar o camelCase, que é a convenção de escrever 
+as variáveis com a primeira letra minúscula e as subsequentes com a primeira letra maiúscula.
+*/
+
+let nome = "João";
+let idade = 27;
+const nomeDoMeuCachorro = "Rex";
+
+console.log("meu nome é", nome, "e tenho", idade, "anos");
+console.log("o nome do meu cachorro é", nomeDoMeuCachorro);
